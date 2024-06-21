cffc230a2a5c22461b33cfb0966f7c59fe937368
”Danny <danny.hearn@outlook.com>
2024-06-21 15:02:29 -0700

    added div

---
+++ script.js
@@ -0,0 +1,37 @@
+
+const newElement = document.createElement('div');
+
+newElement.style.width = '200px';
+newElement.style.height = '200px';
+newElement.style.backgroundColor = 'red';
+
+let container = document.getElementById('box-container');
+    container.appendChild(newElement);
+
+// declaring a new div with .createElement
+// receiving an ID and placing that div into the parent div (newElement) with .appendChild
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+function makeGrid(){
+for (i=0; i <= 16; i++){
+   let newElement =  document.createElement('div');
+   document.body.appendChild(newElement);
+}
+};
+makeGrid();
+
+const container = document.getElementById('box-container');
+*/
+
