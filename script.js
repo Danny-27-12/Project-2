e277d2ff8e14e4d75521b1019f160e7f85bfdbb1
”Danny <danny.hearn@outlook.com>
2024-06-22 17:21:38 -0700

    added a random colour generator

--- script.js
+++ script.js
@@ -16,20 +16,16 @@ makeGrid();
 // adding the for loop allows 16 divs to be created.
 
 
-//setTimeout()
-//clearTimeout()
-
-
 function getHoverPattern() {
 
     let boxes = document.querySelectorAll('.hoverBox');
 
         boxes.forEach((box) => {
             box.addEventListener('mouseover', (event) => {
-                event.target.style.backgroundColor = 'hsl(196, 64%, 43%)';
+                event.target.style.backgroundColor = getRandomColor();
 
                    let timeId = setTimeout( () => {
-                        event.target.style.backgroundColor = 'hsl(170, 64%, 43%)';
+                        event.target.style.backgroundColor = 'rgba(255, 255, 255, 0.85)';
                     }, 500);
                             
             })
@@ -38,14 +34,23 @@ function getHoverPattern() {
 
 getHoverPattern();
 
-    
+//1. state the function
+//2. set up a forEach method that selects each div, adds a 'mouseover' and change the color to random.
+//3. set a timer which converts the div to its original color after a certain period of time.
 
+function getRandomColor() {
+    let color = '#';
+    let choice = '0123456789ABCDEF';
 
+    for (let i=0; i < 6; i++){
+        color += choice[Math.floor(Math.random() * 16)];
+    } 
+    return color;
+};
 
+getRandomColor();
 
-
-//1. querySelectorAll selector that receives the class name.
-//2. we attach an eventListener to that variable with forEach
+console.log(getRandomColor());
 
 
 
